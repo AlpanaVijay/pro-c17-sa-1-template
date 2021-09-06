3ea395798a692567b338a303f017844d57e58b55
AlpanaVijay <70261445+AlpanaVijay@users.noreply.github.com>
2021-09-06 11:54:37 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,11 +2,19 @@
 function setup() 
 {
   createCanvas(400, 400);
+  box1 = new Box(200,100,50,50,2,1);
+  box2 = new Box(300,100,40,40,2,2);
+  
 }
 
 function draw() 
 {
   background(220);
+  box1.show();
+  box1.move();
+  
+  box2.show();
+  box2.moveUp();
 
 }
 
